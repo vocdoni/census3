3763b1fa66a6c26591397fb91464beaca89aad51
p4u <pau@dabax.net>
2023-06-29 15:52:35 +0200

    increase the sleep time between scans
    Signed-off-by: p4u <pau@dabax.net>

--- service/const.go
+++ service/const.go
@@ -3,6 +3,6 @@ package service
 import "time"
 
 const (
-	snapshotBlocks = 100000 // a snapshot and reset of the tree is performed every snapshotBlocks
-	scanSleepTime  = time.Second * 10
+	snapshotBlocks = 100000            // a snapshot and reset of the tree is performed every snapshotBlocks
+	scanSleepTime  = time.Second * 300 // time to sleep between scans, 5 minutes
 )

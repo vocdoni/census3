a28d68e18bab6ec42ae218418d3f328cc7e37f45
Lucas Menéndez <hi@lucasmenendez.me>
2023-09-08 17:54:38 +0200

    updated example

--- example.sh
+++ example.sh
@@ -2,48 +2,54 @@
 
 set -euo pipefail
 
-readonly CONTRACT_ADDRESS="0xa117000000f279D81A1D3cc75430fAA017FA5A2e"
+readonly CONTRACT_ADDRESS="0xFE67A4450907459c3e1FFf623aA927dD4e28c67a"
 readonly CONTRACT_TYPE="erc20"
-readonly BLOCK_NUMBER="11060110"
 readonly API_ENDPOINT="127.0.0.1:7788/api"
 
-add_contract() {
-    curl \
-        --silent \
-        --fail \
-        --show-error \
-        --request POST \
-        --header 'Content-Type: application/json' \
-        --url "${API_ENDPOINT}/addContract/${CONTRACT_ADDRESS}/${CONTRACT_TYPE}/${BLOCK_NUMBER}" \
-        --output /dev/null
+create_token() {
+    curl -X POST \
+        --json "{\"id\": \"$CONTRACT_ADDRESS\",\"type\": \"$CONTRACT_TYPE\",\"chainID\": 1}" \
+        http://$API_ENDPOINT/token
 }
 
-get_contract() {
-    curl \
-        --silent \
-        --fail \
-        --show-error \
-        --request GET \
-        --header 'Content-Type: application/json' \
-        --url "${API_ENDPOINT}/getContract/${CONTRACT_ADDRESS}"
+get_token() {
+    curl -X GET \
+        http://$API_ENDPOINT/token/$CONTRACT_ADDRESS
 }
 
-get_balances() {
-    curl \
-        --silent \
-        --fail \
-        --show-error \
-        --request GET \
-        --header 'Content-Type: application/json' \
-        --url "${API_ENDPOINT}/balances/${CONTRACT_ADDRESS}"
+create_census() {
+    curl -X POST \
+        --json "{\"strategyId\": $1,\"anonymous\": true}" \
+        http://$API_ENDPOINT/census
+}
+
+get_census() {
+    curl -X GET \
+        http://$API_ENDPOINT/census/queue/$1
 }
 
 main() {
-    add_contract
-    sleep 10
-    get_contract
-    sleep 10
-    get_balances
+    # create the token
+    echo "-> creating token..."
+    create_token
+    # wait to be synced (can be checked getting token info)
+    echo "-> created, waiting 4m to token scan"
+    sleep 240
+    # get token info after some scan and store the 'defaultStrategy' id.
+    echo "-> getting token info..."
+    get_token
+    # create the census with the token 'defaultStrategy' id and store the 
+    # resulting 'queueId'.
+    echo "-> enter the strategyId:"
+    read strategyId
+    echo "-> creating census..."
+    create_census $strategyId
+    echo "-> waiting 1m to census publication"
+    sleep 60
+    # get the enqueue census creation process with the 'queueId'
+    echo "-> enter the enqueue census:"
+    read queueId
+    get_census $queueId
 }
 
 main "$@"

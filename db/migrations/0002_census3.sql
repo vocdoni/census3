885295d23d6d32bdff892bad9c733c1140a4edae
Lucas Menéndez <hi@lucasmenendez.me>
2023-11-07 13:11:06 +0100

    bug: old null columns issue with new migration (#111)
    * revert first migration to stage state and update second migration to current service schema, tested with stage database
    * move strategy_tokens table alterations to the first place to the chain_id in the tokens table alterations

--- db/migrations/0002_census3.sql
+++ db/migrations/0002_census3.sql
@@ -1,8 +1,42 @@
 -- +goose Up
 
+-- strategy tokens schema updates
+ALTER TABLE strategy_tokens ADD COLUMN external_id BLOB NOT NULL DEFAULT '';
+CREATE TABLE strategy_tokens_copy (
+    strategy_id INTEGER NOT NULL,
+    token_id BLOB NOT NULL,
+    min_balance BLOB NOT NULL,
+    chain_id INTEGER NOT NULL,
+    external_id TEXT NOT NULL DEFAULT '',
+    PRIMARY KEY (strategy_id, token_id, chain_id, external_id),
+    FOREIGN KEY (strategy_id) REFERENCES strategies(id) ON DELETE CASCADE,
+    FOREIGN KEY (token_id) REFERENCES tokens(id) ON DELETE CASCADE
+);
+INSERT INTO strategy_tokens_copy (strategy_id, token_id, min_balance, chain_id, external_id) 
+SELECT * FROM (
+    SELECT strategy_id, token_id, min_balance, external_id, (
+        SELECT token.chain_id FROM tokens AS token WHERE token.id = strategy_tokens.token_id
+    ) AS chain_id FROM strategy_tokens
+);
+DROP INDEX IF EXISTS idx_strategy_tokens_strategy_id;
+DROP INDEX IF EXISTS idx_strategy_tokens_token_id;
+DROP TABLE strategy_tokens;
+ALTER TABLE strategy_tokens_copy RENAME TO strategy_tokens;
+CREATE INDEX idx_strategy_tokens_strategy_id ON strategy_tokens(strategy_id);
+CREATE INDEX idx_strategy_tokens_token_id ON strategy_tokens(token_id);
+
 -- tokens table schema updates
-ALTER TABLE tokens ADD COLUMN chain_address TEXT;
-ALTER TABLE tokens ADD COLUMN external_id TEXT;
+ALTER TABLE tokens ADD COLUMN chain_address TEXT NOT NULL DEFAULT '';
+ALTER TABLE tokens ADD COLUMN external_id TEXT NOT NULL DEFAULT '';
+ALTER TABLE tokens RENAME COLUMN tag TO tags;
+UPDATE tokens SET name = '' WHERE name IS NULL;
+UPDATE tokens SET symbol = '' WHERE symbol IS NULL;
+UPDATE tokens SET decimals = 0 WHERE decimals IS NULL;
+UPDATE tokens SET total_supply = '' WHERE total_supply IS NULL;
+UPDATE tokens SET creation_block = 0 WHERE creation_block IS NULL;
+UPDATE tokens SET tags = '' WHERE tags IS NULL;
+UPDATE tokens SET chain_address = '' WHERE chain_address IS NULL;
+UPDATE tokens SET external_id = '' WHERE external_id IS NULL;
 CREATE TABLE tokens_copy (
     id BLOB NOT NULL,
     name TEXT NOT NULL DEFAULT '',
@@ -26,8 +60,8 @@ ALTER TABLE tokens_copy RENAME TO tokens;
 CREATE INDEX idx_tokens_type_id ON tokens(type_id);
 
 -- token_holders table schema updates
-ALTER TABLE token_holders ADD COLUMN chain_id INTEGER;
-ALTER TABLE token_holders ADD COLUMN external_id TEXT;
+ALTER TABLE token_holders ADD COLUMN chain_id INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE token_holders ADD COLUMN external_id TEXT NOT NULL DEFAULT '';
 CREATE TABLE token_holders_copy (
     token_id BLOB NOT NULL,
     holder_id BLOB NOT NULL,
@@ -58,29 +92,3 @@ CREATE INDEX idx_token_holders_block_id ON token_holders(block_id);
 -- stategies table schema updates
 ALTER TABLE strategies ADD COLUMN alias TEXT NOT NULL DEFAULT '';
 ALTER TABLE strategies ADD COLUMN uri TEXT NOT NULL DEFAULT '';
-
-ALTER TABLE strategy_tokens ADD COLUMN external_id BLOB;
--- strategy tokens schema updates
-CREATE TABLE strategy_tokens_copy (
-    strategy_id INTEGER NOT NULL,
-    token_id BLOB NOT NULL,
-    min_balance BLOB NOT NULL,
-    chain_id INTEGER NOT NULL,
-    external_id TEXT NOT NULL DEFAULT '',
-    PRIMARY KEY (strategy_id, token_id, external_id),
-    FOREIGN KEY (strategy_id) REFERENCES strategies(id) ON DELETE CASCADE,
-    FOREIGN KEY (token_id) REFERENCES tokens(id) ON DELETE CASCADE
-);
-INSERT INTO strategy_tokens_copy (strategy_id, token_id, min_balance, chain_id, external_id) 
-SELECT * FROM (
-    SELECT strategy_id, token_id, min_balance, external_id, (
-        SELECT token.chain_id FROM tokens AS token WHERE token.id = strategy_tokens.token_id
-    ) AS chain_id FROM strategy_tokens
-);
-
--- DROP INDEX IF EXISTS idx_strategy_tokens_strategy_id;
--- DROP INDEX IF EXISTS idx_strategy_tokens_token_id;
-DROP TABLE strategy_tokens;
-ALTER TABLE strategy_tokens_copy RENAME TO strategy_tokens;
-CREATE INDEX idx_strategy_tokens_strategy_id ON strategy_tokens(strategy_id);
-CREATE INDEX idx_strategy_tokens_token_id ON strategy_tokens(token_id);

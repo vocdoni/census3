b80fd3036eb64d885e7127c662bdcde4560146d0
Lucas Menéndez <hi@lucasmenendez.me>
2024-01-10 10:11:43 +0100

    initial metrics proposals

---
+++ metrics/metrics.go
@@ -0,0 +1,10 @@
+package metrics
+
+// number of tokens
+// number of tokens by type (ERC20, ERC721, ERC1155, ...)
+// number of analysed transfers transactions
+// last analysed block by chain
+// number of strategies
+// number of strategies by type (default or custom)
+// number of censuses
+// number of censuses by type (anonymous or not)

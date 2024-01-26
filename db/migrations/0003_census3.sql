1f1bb1b90c4b6ff087a967f3b52b493c1700ec3c
Lucas Menéndez <hi@lucasmenendez.me>
2024-01-26 17:00:35 +0100

    modify current token types indexes to the new ones in the database migration

--- db/migrations/0003_census3.sql
+++ db/migrations/0003_census3.sql
@@ -2,15 +2,36 @@
 -- +goose Up
 DELETE FROM token_types;
 UPDATE sqlite_sequence SET seq = 0 WHERE name = 'token_types';
-INSERT INTO token_types (type_name) VALUES ('erc20');
-INSERT INTO token_types (type_name) VALUES ('erc721');;
-INSERT INTO token_types (type_name) VALUES ('erc777');
-INSERT INTO token_types (type_name) VALUES ('poap');
+INSERT INTO token_types (id, type_name) VALUES (0, 'unknown');
+INSERT INTO token_types (id, type_name) VALUES (1, 'erc20');
+INSERT INTO token_types (id, type_name) VALUES (2, 'erc721');;
+INSERT INTO token_types (id, type_name) VALUES (3, 'erc777');
+INSERT INTO token_types (id, type_name) VALUES (4, 'poap');
+INSERT INTO token_types (id, type_name) VALUES (5, 'gitcoinpassport');
+
+DELETE FROM tokens WHERE type_id = 3;
+DELETE FROM tokens WHERE type_id = 5;
+DELETE FROM tokens WHERE type_id = 6;
+
+UPDATE tokens SET type_id = 3 WHERE type_id = 4;
+UPDATE tokens SET type_id = 4 WHERE type_id = 8;
+UPDATE tokens SET type_id = 5 WHERE type_id = 100;
 
 ALTER TABLE tokens ADD COLUMN last_block BIGINT NOT NULL DEFAULT 0;
 ALTER TABLE tokens ADD COLUMN analysed_transfers BIGINT NOT NULL DEFAULT 0;
 
 -- List of changes:
 --  * Remove all token types and reset sequence
+--    * Recreate token types with new ids:
+--      * [new] unknown 0
+--      * erc20 1
+--      * erc721 2
+--      * [updated] erc777 from 4 to 3
+--      * [updated] poap from 8 to 4
+--      * [updated] gitcoinpassport from 100 to 5
+--      * [removed] erc1155
+--      * [removed] nation3
+--      * [removed] want
+--      * [removed] erc721_burned
 --  * Add 'last_block' column to 'tokens' table
 --  * Add 'analysed_transfers' column to 'tokens' table

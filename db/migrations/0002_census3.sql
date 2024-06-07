a854048bdbb91a8003a002f75464061fabe2f424
Lucas Menendez <hi@lucasmenendez.me>
2024-06-07 13:20:20 +0200

    include migration down code

--- db/migrations/0002_census3.sql
+++ db/migrations/0002_census3.sql
@@ -12,3 +12,14 @@ SET token_alias = (
     AND (strategy_tokens.external_id = t.external_id OR strategy_tokens.external_id IS NULL AND t.external_id IS NULL)
 )
 WHERE token_alias = '';
+
+-- +goose Down
+DROP INDEX IF EXISTS idx_strategy_tokens_token_alias;
+
+CREATE TABLE strategy_tokens_temp AS
+SELECT strategy_id, token_id, min_balance, chain_id, external_id
+FROM strategy_tokens;
+
+DROP TABLE strategy_tokens;
+
+ALTER TABLE strategy_tokens_temp RENAME TO strategy_tokens;
